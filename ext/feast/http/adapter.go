98c5d96a9e62fc4f96530855ca809a42d5db73e2
mlboy <mlboy@126.com>
2026-01-28 20:49:29 +0800

    update

--- ext/feast/http/adapter.go
+++ ext/feast/http/adapter.go
@@ -325,7 +325,7 @@ func (a *FeatureServiceAdapter) BatchGetRealtimeFeatures(ctx context.Context, pa
 
 // Close 关闭特征服务（实现 core.FeatureService 接口）
 func (a *FeatureServiceAdapter) Close(ctx context.Context) error {
-	// http.Client 接口的 Close() 不接受 context，直接调用即可
+	// http.Client 接口的 Close() 接受 context
 	return a.client.Close(ctx)
 }
 
